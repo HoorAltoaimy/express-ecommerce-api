9be3a22a38cd59eecf7d72906f9ec44c1cd604ae
HOOR <hooraltoaimy@gmail.com>
2023-12-07 04:48:04 +0300

    added new endpoint

--- src/index.js
+++ src/index.js
@@ -4,17 +4,28 @@ const app = express()
 
 const port = 3002
 
+let products = [
+    {id: 1, title: 'iphone 13', price: 1099},
+    {id: 2, title: 'iphone 14', price: 1299},
+    {id: 3, title: 'iphone 15', price: 2099}
+]
+
 app.get('/', (req, res) => {
     res.send('welcome to the express server')
 })
 
 app.get('/products', (req, res) => {
     res.send({
-        products: [
-            {id: 1, title: 'iphone 13', price: 1099},
-            {id: 2, title: 'iphone 14', price: 1299},
-            {id: 3, title: 'iphone 15', price: 2099}
-        ]
+        products,
+    })
+})
+
+app.get('/products/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const product = products.find((product) => product.id === id)
+
+    res.send({
+        product,
     })
 })
 
